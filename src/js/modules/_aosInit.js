cd3546ea49cd5b5d29ecd19287bccf566355a110
Andrey Eferemov <samo.dur.andre@gmail.com>
2023-06-08 12:17:04 +0300

    feat: доделал макет, осталось протестить

--- src/js/modules/_aosInit.js
+++ src/js/modules/_aosInit.js
@@ -1,11 +1,12 @@
-'use strict';
-import AOS from 'aos';
+'use strict'
+import AOS from 'aos'
 
 export function aosInit() {
 	AOS.init({
 		disable: 'mobile',
 		duration: 1000,
 		offset: 1,
-		anchorPlacement: 'top-bottom'
-	});
+		anchorPlacement: 'top-bottom',
+		once: true
+	})
 }
